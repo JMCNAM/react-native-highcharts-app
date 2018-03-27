77f98b999e7e8917a24ed4f232162479acb9c581
James McNamara <32990455+JMCNAM@users.noreply.github.com>
2018-03-28 00:34:26 +0100

    Basic Screen Chart added.
    - BasicChartScreen added.
    - AreaChart added.
    - Index files ajusted.
    - May need refactoring before running.

---
+++ K4ViewChartApp/src/components/charts/AreaChart.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import ChartView from 'react-native-highcharts';
+
+const AreaChart = (props) => {
+  console.log("CHART COMPONENT");
+  console.log(props);
+    var conf={
+        chart: {
+            type: 'area',
+            marginBottom: 100,
+        },
+        title: {
+            text: "Power : Zonal (NORD)",
+            style: {color: "#333333",
+                    fontSize: "20px"}
+        },
+        xAxis: {
+            tickInterval: 1,
+            title: {
+                text: 'Hour'
+            },
+        },
+        yAxis: {
+            title: {
+                text: '€/MH'
+            },
+          },
+        exporting: {
+            enabled: false
+        },
+        plotOptions: {
+              series: {
+                  color: "#617685",
+                  fillOpacity: 0.2
+              }
+        },
+        series: [{
+            name: 'Power Houly Prices',
+            data: props.series,
+        }]
+    };
+    return(
+      <View style={styles.container}>
+        <ChartView style={{height:"90%"}} config={conf}/>
+      </View>
+    );
+}
+const styles = {
+  container: {
+    flex:1,
+    flexGrow: 1
+  },
+}
+export {AreaChart};
